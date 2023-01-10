ad7264d741e9ff3e8f12ead266947ad3f0b31cce
tonmoy <tonmoy3673@gmail.com>
2023-01-11 00:52:32 +0600

    built error page

--- src/pages/shared/ErrorPage/ErrorPage.jsx
+++ src/pages/shared/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useRouteError } from 'react-router-dom';
 
 const ErrorPage = () => {
+    const error=useRouteError();
     return (
-        <div>
-            <h2>This is error page</h2>
+        <div className='py-4 text-center w-screen h-screen flex items-center justify-center'>
+            <div className=''>
+            <h2 className='text-3xl font-bold py-3'>Oops! An Error Occurred 😢</h2>
+            {error && (
+          <div>
+            
+            <h5 className='text-xl text-red-600 font-semibold'>{error.status}</h5>
+            <h5 className='text-2xl text-red-600 font-semibold'>{error.statusText || error.message}</h5>
+          </div>
+        )}
+            </div>
         </div>
     );
 };
