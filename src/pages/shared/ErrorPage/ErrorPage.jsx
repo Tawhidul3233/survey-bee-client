cc22cf29164ad95513a66df7bd22fa10397d9943
tonmoy <tonmoy3673@gmail.com>
2023-01-11 01:00:56 +0600

    built error page

--- src/pages/shared/ErrorPage/ErrorPage.jsx
+++ src/pages/shared/ErrorPage/ErrorPage.jsx
@@ -9,8 +9,8 @@ const ErrorPage = () => {
             <h2 className='text-3xl font-bold py-3'>Oops! An Error Occurred 😢</h2>
             {error && (
           <div>
-            
             <h5 className='text-xl text-red-600 font-semibold'>{error.status}</h5>
+            
             <h5 className='text-2xl text-red-600 font-semibold'>{error.statusText || error.message}</h5>
           </div>
         )}
