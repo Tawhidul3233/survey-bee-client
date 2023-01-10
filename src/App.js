949010a9526f0dc43b006a95ea452e876931a17b
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2023-01-10 17:57:37 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+    <h1 className='text-center'> Hello Dear </h1>
+    </div>
+  );
+}
+
+export default App;
