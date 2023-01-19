0d8f792f57ffbf20f7c14fa30d99dc51336d5f73
oliullah <razasm29@gmail.com>
2023-01-20 01:32:06 +0600

    created modal for upgrade user profile in dashboard and show user name instead of user email in dashboard

--- src/index.js
+++ src/index.js
@@ -6,9 +6,15 @@ import "./customCSS/custom.css";
 import store from "./app/store";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// tanstack queryClient
 const queryClient = new QueryClient();
+
+
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+
+
 root.render(
   <Provider store={store}>
     <React.StrictMode>
--- src/layouts/Dashboard/Dashboard.jsx
+++ src/layouts/Dashboard/Dashboard.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
+import ScrollToTop from "../../utils/ScrollToTop/ScrollToTop";
 
 const Dashboard = () => {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <Outlet />
       <Footer />
