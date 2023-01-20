153368971c5817c2dc99718f575454126a02e2bf
oliullah <razasm29@gmail.com>
2023-01-20 20:03:40 +0600

    add simple loading

---
+++ src/components/Shared/Loading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center mt-36">
+      <p className="text-info w-44 h-16 text-xl bg-black text-center py-1 px-2 rounded-md">
+        Loading, please wait...
+      </p>
+    </div>
+  );
+};
+
+export default Loading;
