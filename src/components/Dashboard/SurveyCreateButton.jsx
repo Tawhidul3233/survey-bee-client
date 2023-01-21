7a454660bd8b4ab82359bd12e87d1f804ac5a283
oliullah <razasm29@gmail.com>
2023-01-21 21:12:05 +0600

    add create survey modal

--- src/components/Dashboard/SurveyCreateButton.jsx
+++ src/components/Dashboard/SurveyCreateButton.jsx
@@ -8,7 +8,10 @@ const SurveyCreateButton = () => {
         <h2 className="text-2xl py-10">
           Let's get started & create your first survey!
         </h2>
-        <Link to="/dashboard/createasurvey" className="btn btn-primary text-white px-10">
+        <Link
+          to="/dashboard/createasurvey"
+          className="btn btn-primary text-white px-10"
+        >
           Create Survey
         </Link>
       </div>
--- src/pages/Dashboard/CreateASurvey.jsx
+++ src/pages/Dashboard/CreateASurvey.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import CreateASurveyModal from "../../components/Dashboard/CreateASurveyModal";
 
 const CreateASurvey = () => {
+  const [userSurveyTitle, setUserSurveyTitle] = useState("");
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  // create a survey in createASurveyModal
+  const handleCreateASurvey = (data) => {
+    console.log(data);
+    setUserSurveyTitle(data?.surveyTitle);
+  };
+
   return (
     <div className="min-h-screen">
       <div className="flex flex-col items-center my-6">
@@ -10,21 +25,34 @@ const CreateASurvey = () => {
           How would you like to get started?
         </h2>
         <div className="flex mt-6 gap-x-8">
-          <div className="card w-80 h-96 bg-base-100 shadow-xl hover:shadow-2xl cursor-pointer py-10">
-            <figure className="px-10 pt-10">
-              <img
-                src="https://i.ibb.co/9scLw4z/surv-create-ic-png.png"
-                alt="create survey icon"
-                className="w-2/4 h-autho"
-              />
-            </figure>
-            <div className="card-body items-center text-center pt-6">
-              <h2 className="card-title text-secondary font-bold">
-                Start your survey from scratch
-              </h2>
-              <p className="pt-3">Build your survey from scratch.</p>
+          <label
+            htmlFor="createSurveyModal"
+            onClick={() => setUserSurveyTitle("")}
+          >
+            <div className="card w-80 h-96 bg-base-100 shadow-xl hover:shadow-2xl cursor-pointer py-10">
+              <figure className="px-10 pt-10">
+                <img
+                  src="https://i.ibb.co/9scLw4z/surv-create-ic-png.png"
+                  alt="create survey icon"
+                  className="w-2/4 h-autho"
+                />
+              </figure>
+              <div className="card-body items-center text-center pt-6">
+                <h2 className="card-title text-secondary font-bold">
+                  Start your survey from scratch
+                </h2>
+                <p className="pt-3">Build your survey from scratch.</p>
+              </div>
             </div>
-          </div>
+          </label>
+          {!userSurveyTitle && (
+            <CreateASurveyModal
+              register={register}
+              errors={errors}
+              handleSubmit={handleSubmit}
+              handleCreateASurvey={handleCreateASurvey}
+            />
+          )}
           <div className="card w-80 h-96 bg-base-100 shadow-xl hover:shadow-2xl cursor-pointer py-10">
             <figure className="px-10 pt-10">
               <img
