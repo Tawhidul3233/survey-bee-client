40df7292ce409e0fdf08fb06e873cbee0b75f733
Tuhin Hossain <95519628+tuhin180@users.noreply.github.com>
2023-02-09 17:53:06 +0600

    done with aurvey audience

--- src/components/SurveyAudience/AudienceCategory/CardItem.jsx
+++ src/components/SurveyAudience/AudienceCategory/CardItem.jsx
@@ -1,17 +1,17 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
-const CardItem = ({ card, selectedCategoryId }) => {
+const CardItem = ({ card, selectedCategoryId, isFirstCard }) => {
   const { img, title, desc } = card;
+
   return (
-    <div>
-      <Link
-        style={{ backgroundImage: `url(${img})` }}
-        to={`/survey-Audience/${title}/${selectedCategoryId}`}
-        className="relative block overflow-hidden rounded-xl bg-cover bg-center bg-no-repeat transform 
-        transition duration-1000 hover:scale-110"
-      >
-        <div className="relative bg-black bg-opacity-40 p-8 pt-40 text-white">
+    <div
+      style={{ backgroundImage: `url(${img})` }}
+      className={`relative block overflow-hidden rounded-xl bg-cover bg-center bg-no-repeat transform transition duration-1000 hover:scale-110 ${
+        isFirstCard ? "first-card" : ""
+      }`}
+    >
+      <Link to={`/survey-Audience/${title}/${selectedCategoryId}`}>
+        <div className="relative bg-black bg-opacity-40 p-8 pt-40 text-white flex flex-col justify-center items-center">
           <h3 className="text-2xl font-bold">{title}</h3>
 
           <p className="text-sm">{desc}</p>
@@ -20,5 +20,4 @@ const CardItem = ({ card, selectedCategoryId }) => {
     </div>
   );
 };
-
 export default CardItem;
--- src/pages/SurveyAudience/SurveyAudience.jsx
+++ src/pages/SurveyAudience/SurveyAudience.jsx
@@ -1,15 +1,57 @@
-import React from "react";
 import { useLoaderData } from "react-router";
 
 const SurveyAudience = () => {
   const cards = useLoaderData();
-  console.log(cards[0]);
+  const survey = cards[0].survey[0];
+
   return (
-    <>
-      <div className="my-3 text-center">
-        <h1 className="text-xl">{cards[0].title}</h1>
-      </div>
-    </>
+    <div className="p-6 bg-white rounded-lg shadow-lg">
+      <h2 className="text-2xl font-medium mb-4">{survey.survey_title}</h2>
+      <form>
+        {survey.questions.map((question) => (
+          <div key={question.question_id} className="mb-6">
+            <p className="text-lg font-medium mb-2">{question.question_text}</p>
+            {question.answer_type === "rating" && (
+              <div className="flex items-center">
+                {question.options.map((option) => (
+                  <label key={option} className="flex items-center mr-4">
+                    <input
+                      type="radio"
+                      className="form-radio"
+                      name={`question-${question.question_id}`}
+                    />
+                    <span className="ml-2">{option}</span>
+                  </label>
+                ))}
+              </div>
+            )}
+            {question.answer_type === "yesno" && (
+              <div className="flex items-center">
+                {question.options.map((option) => (
+                  <label key={option} className="flex items-center mr-4">
+                    <input
+                      type="radio"
+                      className="form-radio"
+                      name={`question-${question.question_id}`}
+                    />
+                    <span className="ml-2">{option}</span>
+                  </label>
+                ))}
+              </div>
+            )}
+            {question.answer_type === "text" && (
+              <textarea
+                className="form-input w-full mt-2"
+                name={`question-${question.question_id}`}
+              />
+            )}
+          </div>
+        ))}
+        <button className="btn bg-blue-500 text-white py-2 px-4 rounded-lg">
+          Submit
+        </button>
+      </form>
+    </div>
   );
 };
 
