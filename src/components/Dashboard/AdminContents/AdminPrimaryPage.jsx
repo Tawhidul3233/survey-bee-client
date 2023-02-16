c24becb4e865df8a90627f341f43946b4afe9e4a
Tuhin Hossain <95519628+tuhin180@users.noreply.github.com>
2023-02-16 14:38:17 +0600

    done without error

--- src/components/Dashboard/AdminContents/AdminPrimaryPage.jsx
+++ src/components/Dashboard/AdminContents/AdminPrimaryPage.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-<<<<<<< HEAD
 
-=======
-import { Outlet } from "react-router-dom";
-import Footer from "../../Footer";
-import Navbar from "../../Navbar";
-import MenuTest from "../MenuTest";
-import IncameChart from "./AdminComponents/IncameChart";
-import UserChart from "./AdminComponents/UserChart";
-import UserStatstics from "./AdminComponents/UserStatstics";
-import AdminMenu from "./AdminMenu/AdminMenu";
->>>>>>> ec275f164068c554d932cf3ee4ae62a5de57ec5a
+// import { Outlet } from "react-router-dom";
+// import Footer from "../../Footer";
+// import Navbar from "../../Navbar";
+// import MenuTest from "../MenuTest";
+// import IncameChart from "./AdminComponents/IncameChart";
+// import UserChart from "./AdminComponents/UserChart";
+// import UserStatstics from "./AdminComponents/UserStatstics";
+// import AdminMenu from "./AdminMenu/AdminMenu";
+
 import AdminHome from "./ManageUser/AdminHome/AdminHome";
 
 const AdminPrimaryPage = () => {
