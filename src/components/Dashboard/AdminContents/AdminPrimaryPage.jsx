c643590e0a68610f50bf93c2e9192f37e0c5ca50
Amran Hossain <amran.h.akash@gmail.com>
2023-02-17 15:43:14 +0600

    something

--- src/components/Dashboard/AdminContents/AdminPrimaryPage.jsx
+++ src/components/Dashboard/AdminContents/AdminPrimaryPage.jsx
@@ -1,3 +1,14 @@
+import React from "react";
+
+// import { Outlet } from "react-router-dom";
+// import Footer from "../../Footer";
+// import Navbar from "../../Navbar";
+// import MenuTest from "../MenuTest";
+// import IncameChart from "./AdminComponents/IncameChart";
+// import UserChart from "./AdminComponents/UserChart";
+// import UserStatstics from "./AdminComponents/UserStatstics";
+// import AdminMenu from "./AdminMenu/AdminMenu";
+
 import AdminHome from "./ManageUser/AdminHome/AdminHome";
 
 const AdminPrimaryPage = () => {
