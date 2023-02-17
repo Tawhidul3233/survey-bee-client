fd980f6337f443b1e464e089a2075fb85c6515de
Amran Hossain <amran.h.akash@gmail.com>
2023-02-17 15:40:01 +0600

    something

--- src/components/Dashboard/AdminContents/AdminPrimaryPage.jsx
+++ src/components/Dashboard/AdminContents/AdminPrimaryPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Footer from "../../Shared/Footer";
+import Footer from "../../Footer";
 import Navbar from "../../Navbar";
 import MenuTest from "../MenuTest";
 import IncameChart from "./AdminComponents/IncameChart";
