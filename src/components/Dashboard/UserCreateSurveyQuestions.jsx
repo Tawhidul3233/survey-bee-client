1385eae1923f973253ba31864ea193041015e139
oliullah <razasm29@gmail.com>
2023-02-22 21:17:33 +0600

    fix a bug

--- src/components/Dashboard/UserCreateSurveyQuestions.jsx
+++ src/components/Dashboard/UserCreateSurveyQuestions.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import Loading from "../Shared/Loading";
 
-const UserCreateSurveyQuestions = ({ userCreatedQuestion, handleDeleteSurveyQuestion,
-  editSurveyLoaderData }) => {
-  const surveyInfo = editSurveyLoaderData || userCreatedQuestion[0];
+const UserCreateSurveyQuestions = ({
+  userCreatedQuestion,
+  handleDeleteSurveyQuestion,
+  editSurveyLoaderData,
+}) => {
+  // const surveyInfo = editSurveyLoaderData || userCreatedQuestion[0];
+  let surveyInfo;
+  if (editSurveyLoaderData?.surveyTitle) {
+    surveyInfo = editSurveyLoaderData;
+  } else {
+    surveyInfo = userCreatedQuestion[0];
+  }
   // console.log(editSurveyLoaderData, "----------", userCreatedQuestion[0]);
   // console.log(surveyInfo?.questionsAndTypes?.questionsType)
   // console.log(surveyInfo?.questionsAndTypes?.optionAnswers)
+  
   return (
     <>
       <div className="flex flex-col items-start gap-y-10">
         {surveyInfo?.questionsAndTypes?.map((q, i) => {
           return (
             <>
+              {/* {console.log("I am from jsx", q?.questions)} */}
               {q?.questions ? (
                 <div
                   className="w-full text-start h-auto cursor-pointer flex flex-col gap-y-6 px-1 py-2
@@ -38,24 +49,29 @@ const UserCreateSurveyQuestions = ({ userCreatedQuestion, handleDeleteSurveyQues
                       </button>
                     </>
                   </div>
-                  {q?.questionsType === "Comment Box" && (
+                  {(q?.questionsType === "Comment Box" && (
                     <textarea
                       className=" border outline-none  w-full sm:w-10/12 py-2 px-4 resize-none cursor-pointer rounded-sm"
                       placeholder="Write your opinion:.."
                     ></textarea>
-                  ) || undefined || q?.questionsType === "Textbox" && (
-                    <input
-                      type="text"
-                      className="border outline-none  w-full sm:w-10/12 py-2 px-4  cursor-pointer rounded-sm"
-                      placeholder="Answer:.."
-                    />
-                  ) || undefined || q?.questionsType === "Multiple choice" &&
-
-                    <>
-                      <div className="mb-4 grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-1  w-full sm:w-10/12 ">
-                        {
-                          q?.optionAnswers?.map((option, index) => (
-                            <div key={index} className="mb-4 bg-success text-white rounded-sm ">
+                  )) ||
+                    undefined ||
+                    (q?.questionsType === "Textbox" && (
+                      <input
+                        type="text"
+                        className="border outline-none  w-full sm:w-10/12 py-2 px-4  cursor-pointer rounded-sm"
+                        placeholder="Answer:.."
+                      />
+                    )) ||
+                    undefined ||
+                    (q?.questionsType === "Multiple choice" && (
+                      <>
+                        <div className="mb-4 grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-1  w-full sm:w-10/12 ">
+                          {q?.optionAnswers?.map((option, index) => (
+                            <div
+                              key={index}
+                              className="mb-4 bg-success text-white rounded-sm "
+                            >
                               <input
                                 type="radio"
                                 id={option}
@@ -66,12 +82,11 @@ const UserCreateSurveyQuestions = ({ userCreatedQuestion, handleDeleteSurveyQues
                                 {option.answer}
                               </label>
                             </div>
-                          ))
-                        }
-                      </div>
-                    </>
-
-                    || undefined}
+                          ))}
+                        </div>
+                      </>
+                    )) ||
+                    undefined}
                 </div>
               ) : null}
             </>
