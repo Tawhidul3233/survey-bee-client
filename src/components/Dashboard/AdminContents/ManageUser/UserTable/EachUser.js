3fdf1c6d956ab9b2755b7c2f1fadaa107552bb2b
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2023-02-23 13:06:59 +0600

    user remove and desing add

--- src/components/Dashboard/AdminContents/ManageUser/UserTable/EachUser.js
+++ src/components/Dashboard/AdminContents/ManageUser/UserTable/EachUser.js
@@ -1,21 +1,28 @@
 import React from 'react';
-
+import { useState } from 'react';
+import UserDeleteModal from './UserDeleteModal';
 const EachUser = ({ d }) => {
+  const [deleteUser, setDeleteUser] = useState({})
   // console.log(d)
   return (
     <>
       <tr>
-        <td> {d._id}    </td>
-        <td> {d.userName}  </td>
-        <td> {d.email} </td>
-        <td> {d.jobRole}  </td>
-        <td> {d.userType}  </td>
+        <td> {d?._id}    </td>
+        <td> {d?.userName}  </td>
+        <td> {d?.email} </td>
+        <td> {d?.jobRole}  </td>
+        <td> {d?.userType}  </td>
         <td >
-          <button className='btn btn-xs bg-red-600 text-white border-none text-[10px]'> Remove </button>
+          {/* <button onClick={() => setDeleteUser(d)} className='btn btn-xs bg-red-600 text-white border-none text-[10px]'> Remove </button> */}
+          <label htmlFor="delete-modal" className='ml-2 btn btn-xs btn-outline bg-red-500 text-white' onClick={() => setDeleteUser(d)}>Remove
+            {/* onClick={() => deleteItem(bill._id)} */}
+          </label>
+          <UserDeleteModal d={d}> </UserDeleteModal>
         </td>
       </tr>
     </>
   );
 };
+
 
 export default EachUser;
