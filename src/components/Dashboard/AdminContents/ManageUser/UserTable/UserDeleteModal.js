4b6c4343de163f93748bb23672fe2bd700a2702f
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2023-02-23 15:02:03 +0600

    user problem solve

--- src/components/Dashboard/AdminContents/ManageUser/UserTable/UserDeleteModal.js
+++ src/components/Dashboard/AdminContents/ManageUser/UserTable/UserDeleteModal.js
@@ -2,19 +2,18 @@ import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { toast } from 'react-hot-toast';
 
-const UserDeleteModal = ({ d }) => {
-  // console.log(d._id)
-  // Delete click Handler and delete bills
-
+const UserDeleteModal = ({ _id, userName }) => {
+// console.log(_id, userName)
   const deleteItem = (id) => {
-    fetch(`https://survey-bee-server.vercel.app/users/${id}`,
+    console.log(id)
+    fetch(`https://power-hack-server-green.vercel.app/api/delete-billing/${id}`,
       {
         method: 'DELETE',
       })
       .then(res => res.json())
       .then(data => {
         console.log(data)
-        toast.success(`successfully Deleted ${d?.userName}` )
+        toast.success('Bill successfully Deleted')
       })
       .catch(error => {
         console.log(error)
@@ -27,16 +26,17 @@ const UserDeleteModal = ({ d }) => {
       <input type="checkbox" id="delete-modal" className="modal-toggle" />
       <div className="modal ">
         <div className="modal-box w-1/4">
-          <p className="font-bold  text-center"> Are you sure you want to delete? </p>
-          <p className=' text-center'>{d?.userName}</p>
+          <p className="font-bold text-center">Are you sure you want to delete?</p>
+          <p> {userName} </p>
           <div className="modal-action justify-center">
-            <label onClick={() => deleteItem(d._id)} htmlFor="delete-modal" className="btn btn-sm bg-red-600 text-white">Yes</label>
-            <label htmlFor="delete-modal" className="btn btn-sm bg-black text-white">No</label>
+            <label onClick={() => deleteItem(_id)} htmlFor="delete-modal" className="btn bg-red-500">Yes</label>
+            <label htmlFor="delete-modal" className="btn">No</label>
           </div>
         </div>
       </div>
     </div>
   );
 };
+
 
 export default UserDeleteModal;
