7ee65435a14183efe66dd64fc8ee5ba8a9efe993
Tuhin Hossain <95519628+tuhin180@users.noreply.github.com>
2023-02-24 13:14:16 +0600

    done

--- src/pages/shared/Login/Login.js
+++ src/pages/shared/Login/Login.js
@@ -5,7 +5,6 @@ import { toast } from "react-hot-toast";
 import { useForm } from "react-hook-form";
 import { useLocation, useNavigate } from "react-router-dom";
 import GoogleLogin from "../../../components/Navbar/GoogleLogin";
-import SmallSpinner from "../../../components/Spinner/SmallSpinner";
 
 const Login = () => {
   const { register, handleSubmit } = useForm();
